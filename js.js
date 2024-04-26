fc67e281e7f668724094409e6bbe8dcce4f80a79
biafrm <biancafreiremarquess@gmail.com>
2024-04-26 19:06:59 -0300

    faq

--- js.js
+++ js.js
@@ -8,3 +8,24 @@ function fecharPopUp() {
     var popUp = document.getElementById("pop-up");
     popUp.style.display = "none";
 }
+
+
+function toggleFAQ(index) {
+    var faqs = document.getElementsByClassName("faq-item");
+    for (let i = 0; i < faqs.length; i++) {
+        var content = faqs[i].getElementsByClassName("faq-content")[0];
+        var sign = faqs[i].getElementsByClassName("toggle-sign")[0];
+        if (i === index) {
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+                sign.textContent = '+';
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+                sign.textContent = '-';
+            }
+        } else {
+            content.style.maxHeight = null;
+            sign.textContent = '+';
+        }
+    }
+}
